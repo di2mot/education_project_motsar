4d585685b196420da246efb051636884d1cf773b
Dima Motsar <motsar.dm@gmail.com>
2021-11-10 21:15:38 +0200

    split the model file into parts

---
+++ Filmbook/filmBook/app_models/directors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+"""Database app_models classes for Filmbook app"""
+# =============================================================================
+# Imports
+# =============================================================================
+from filmBook import db
+from associatin_tables import films_directors
+
+
+class Directors(db.Model):
+    """Table with genres"""
+
+    __tablename__ = 'directors'
+    director_d = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), unique=True, nullable=False)
+    second_name = db.Column(db.String(20), unique=True, nullable=False)
+    film = db.relationship('films', secondary=films_directors, lazy='subquery',
+                           backref=db.backref('directors', lazy=True))
+
+    def __init__(self, first_name, second_name):
+        self.first_name = first_name
+        self.second_name = second_name
+
+    def __repr__(self):
+        return f'<Directors: first name: {self.first_name}, second name: {self.second_name}>'
+        # return "<{0.__class__.__name__}(id={0.id!r})>".format(self)
