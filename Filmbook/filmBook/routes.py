74f6ca38cf5d6f5d3dcd8d094386d43353c55ffb
Dima Motsar <motsar.dm@gmail.com>
2021-11-26 23:20:00 +0200

    code refactoring

--- Filmbook/filmBook/routes.py
+++ Filmbook/filmBook/routes.py
@@ -5,7 +5,6 @@
 # =============================================================================
 # Imports
 # =============================================================================
-from flask import request, jsonify
 from flask_login import current_user, login_user, login_required, logout_user
 from flask_restx import Resource
 from filmBook import api, db_core, app
@@ -17,18 +16,22 @@ class FilmRout(Resource):
     """Rout with filter"""
 
     @api.marshal_with(resources.films_model, code=200, envelope='films', as_list=True)
+    @api.expect(resources.films_get_parser)
+    @api.doc("Search film in db")
     def get(self):
+        """
+        Search film in db
+        Example request
+        http://127.0.0.1:5000/films?page=1&name=a&genre=Drama"""
 
-        """http://127.0.0.1:5000/films?page=1&name=a&genre=Drama&
-        rel_start=1980&rel_fin=2020&director=Christopher Nolan"""
         message = resources.films_get_parser.parse_args()
 
         if current_user.is_anonymous:
-            id = None
+            user_id = None
         else:
-            id = current_user.id
+            user_id = current_user.id
 
-        app.logger.info(f"location:'/add_film', user_id={id}, message: {message}")
+        app.logger.info(f"location:'/add_film', user_id={user_id}, message: {message}")
 
         res = db_core.get_full_film_info(
                                          film_name=message['name'],
@@ -43,13 +46,15 @@ class FilmRout(Resource):
 
     @api.marshal_with(resources.delete_films_model, code=200, envelope='films', as_list=True)
     @login_required
+    @api.expect(resources.del_films_parser)
+    @api.doc("Delete movie from db")
     def delete(self):
-        del_films_parser = api.parser()
-        del_films_parser.add_argument('film_id', type=int)
-        message = del_films_parser.parse_args()
-        id = current_user.id
-        app.logger.info(f"user_id={id}, message: {message}")
-        return db_core.delete_film(film_id=message['film_id'], user_id=id)
+        """Delete movie from db"""
+
+        message = resources.del_films_parser.parse_args()
+        user_id = current_user.id
+        app.logger.info(f"user_id={user_id}, message: {message}")
+        return db_core.delete_film(film_id=message['film_id'], user_id=user_id)
 
 
 @api.route('/add_film')
@@ -58,11 +63,15 @@ class AddFilmRout(Resource):
 
     @api.marshal_with(resources.add_films_model, code=200, envelope='new_films', as_list=True)
     @login_required
+    @api.expect(resources.add_films_post_parser)
+    @api.doc("Add film to the database")
     def post(self):
+        """Add film to the database"""
+
         message = resources.add_films_post_parser.parse_args()
 
-        id = current_user.id
-        app.logger.info(f"location:'/add_film', user_id={id}, message: {message}")
+        user_id = current_user.id
+        app.logger.info(f"location:'/add_film', user_id={user_id}, message: {message}")
         res = db_core.add_new_film(
                                         imdb_id=message["imdb_id"],
                                         film_name=message["film_name"],
@@ -70,7 +79,7 @@ class AddFilmRout(Resource):
                                         poster_url=message["poster_url"],
                                         release_date=message["release_date"],
                                         rating=message["rating"],
-                                        user_added=id,
+                                        user_added=user_id,
                                         genre=message["genre"],
                                         director=message["director"],
                                         description=message["description"],
@@ -80,12 +89,15 @@ class AddFilmRout(Resource):
 
     @api.marshal_with(resources.update_films_model, code=200, envelope='update_films', as_list=True)
     @login_required
+    @api.expect(resources.update_films_parser)
+    @api.doc("Update film in the database")
     def put(self):
+        """Update film in the database"""
 
         message = resources.update_films_parser.parse_args()
 
-        id = current_user.id
-        app.logger.info(f"location:'/add_film', user_id={id}, message: {message}")
+        user_id = current_user.id
+        app.logger.info(f"location:'/add_film', user_id={user_id}, message: {message}")
         res = db_core.update_film(
                                         film_id=message["film_id"],
                                         imdb_id=message["imdb_id"],
@@ -94,7 +106,7 @@ class AddFilmRout(Resource):
                                         poster_url=message["poster_url"],
                                         release_date=message["release_date"],
                                         rating=message["rating"],
-                                        user_added=id,
+                                        user_added=user_id,
                                         genre=message["genre"],
                                         director=message["director"],
                                         description=message["description"],
@@ -105,24 +117,34 @@ class AddFilmRout(Resource):
 
 @api.route('/director')
 class DelDirector(Resource):
+    """For directors delete"""
 
     @api.marshal_with(resources.dir_name_model, code=200, envelope='director', as_list=True)
     @login_required
+    @api.expect(resources.dir_name_parser)
+    @api.doc("Delete movie director from the database")
     def delete(self):
-        id = current_user.id
+        """Delete movie director from the database"""
+
+        user_id = current_user.id
         message = resources.dir_name_parser.parse_args()
 
-        app.logger.info(f"location:'/director', user_id={id}, message: {message}")
+        app.logger.info(f"location:'/director', user_id={user_id}, message: {message}")
 
-        res = db_core.delete_directors(director_name=message['name'], user_id=id)
+        res = db_core.delete_directors(director_name=message['name'], user_id=user_id)
 
         return res
 
 
 @api.route('/registration')
 class UserRegistration(Resource):
+    """User authentication class"""
+
     @api.marshal_with(resources.add_user_model, code=200, envelope='new_user', as_list=True)
+    @api.expect(resources.add_user_post_parser)
+    @api.doc("Registration for new users at the server")
     def post(self):
+        """Registration for new users at the server"""
 
         message = resources.add_user_post_parser.parse_args()
         app.logger.info(f"location:'/registration', user_id={None}, message: {message}")
@@ -136,9 +158,13 @@ class UserRegistration(Resource):
 
 @api.route('/login')
 class UserLogin(Resource):
-
+    """User authentication class"""
+    
     @api.marshal_with(resources.login_user_model, code=200, envelope='login', as_list=True)
+    @api.expect(resources.login_user_post_parser)
+    @api.doc("Login at the server")
     def post(self):
+        """Login at the server"""
 
         message = resources.login_user_post_parser.parse_args()
         app.logger.info(f"location:'/login', user_id={None}, message: {message['nickname']}")
@@ -154,11 +180,15 @@ class UserLogin(Resource):
 
 @api.route("/logout")
 class UserLogout(Resource):
+    """User authentication class"""
 
     @login_required
     @api.marshal_with(resources.logout_user_model, code=200, envelope="logout")
+    @api.doc("Logout current user from the server")
     def post(self):
-        id = current_user.id
-        app.logger.info(f"location:'/logout', user_id={id}, message: {None}")
+        """Logout current user from the server"""
+
+        user_id = current_user.id
+        app.logger.info(f"location:'/logout', user_id={user_id}, message: {None}")
         logout_user()
-        return {"id": int(id), "status": True}
+        return {"id": int(user_id), "status": True}
